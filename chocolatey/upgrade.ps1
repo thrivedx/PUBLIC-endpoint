089812bd1d6f6488a009b4af064e49ecea0a05fe
Dan <44279121+dsmith73@users.noreply.github.com>
2022-08-20 10:41:23 -0400

    Create upgrade.ps1

---
+++ chocolatey/upgrade.ps1
@@ -0,0 +1,30 @@
+###########################  
+# upgrade applications  
+# with chocolatey  
+###########################  
+
+# Slack  
+choco upgrade slack -a
+
+# MS Teams  
+# choco upgrade microsoft-teams.install
+
+# VLC Media Player  
+choco upgrade vlc
+
+# Zoom  
+choco upgrade zoom
+
+# SysInternals  
+# choco install sysinternals
+
+# Google Drive  
+# choco upgrade googledrive
+
+
+# Chrome Extensions ###########  
+## Google Calendar  
+choco upgrade google-calendar-chrome
+
+## BitWarden  
+choco upgrade bitwarden-chrome
