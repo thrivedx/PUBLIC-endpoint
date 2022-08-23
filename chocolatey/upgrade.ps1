2a5a9034440dfab17e92081f3710eed4e5ba9e32
Dan <44279121+dsmith73@users.noreply.github.com>
2022-08-23 06:27:46 -0400

    Update upgrade.ps1

--- chocolatey/upgrade.ps1
+++ chocolatey/upgrade.ps1
@@ -1,13 +1,15 @@
-###########################  
-# upgrade applications  
-# with chocolatey  
-###########################  
+# upgrade apps with chocolatey  
+# this file is intended to be stored in a central location  
+# where users can access it programatically to keep certain apps up to date  
+
+# chocolatey  
+choco upgrade chocolatey
 
 # Slack  
-choco upgrade slack -a
+choco upgrade slack
 
 # MS Teams  
-# choco upgrade microsoft-teams.install
+choco upgrade microsoft-teams.install
 
 # VLC Media Player  
 choco upgrade vlc
@@ -16,21 +18,13 @@ choco upgrade vlc
 choco upgrade zoom
 
 # SysInternals  
-# choco install sysinternals
+# choco upgrade sysinternals
 
-# Google Drive  
-# choco upgrade googledrive
+# Google  
+    # - Drive  
+choco upgrade googledrive
+    # - chrome  
+choco upgrade googlechrome
 
-# .NET  
-choco upgrade dotnet-6.0-runtime -y
-
-# ESET  
-# choco upgrade eset-antivirus
-
-
-# Chrome Extensions ###########  
-## Google Calendar  
-choco upgrade google-calendar-chrome
-
-## BitWarden  
-choco upgrade bitwarden-chrome
+# Microsoft Edge  
+choco upgrade microsoft-edge
