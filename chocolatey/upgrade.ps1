6343e7f807be6a99e809c31df8df84245796baf4
Dan <44279121+dsmith73@users.noreply.github.com>
2024-06-08 12:04:42 -0400

    Update upgrade.ps1

--- chocolatey/upgrade.ps1
+++ chocolatey/upgrade.ps1
@@ -5,26 +5,78 @@
 # chocolatey  
 choco upgrade chocolatey
 
+
 # Slack  
 choco upgrade slack
 
-# MS Teams  
-choco upgrade microsoft-teams.install
 
-# VLC Media Player  
-choco upgrade vlc
+# # MS Teams  
+# choco upgrade microsoft-teams.install
+
+
+# # VLC Media Player  
+# choco upgrade vlc
+
 
 # Zoom  
 choco upgrade zoom
 
+
 # SysInternals  
 # choco upgrade sysinternals
+
 
 # Google  
     # - Drive  
 choco upgrade googledrive
+    # - command line utility for interacting with Google Drive  
+    # https://github.com/glotlabs/gdrive
+choco upgrade gdrive
     # - chrome  
 choco upgrade googlechrome
+    # - UBlock Origin
+choco upgrade ublockorigin-chrome
+    # - Voice  
+choco upgrade google-voice-chrome
+
 
 # Microsoft Edge  
 choco upgrade microsoft-edge
+
+
+# # OpenVPN  
+# choco upgrade openvpn-connect
+
+
+# # BitWarden  
+#     # - Local  
+# choco upgrade bitwarden
+#     # - chrome  
+# choco upgrade bitwarden-chrome  
+#     # - CLI  
+# choco upgrade bitwarden-cli
+
+
+# # ESET  
+# choco upgrade eset-internet-security
+
+
+# DrawIO  
+choco upgrade drawio
+
+
+# # Docker Desktop  
+# choco upgrade docker-desktop
+
+
+# # Firefox  
+# choco upgrade firefox
+
+
+
+
+
+
+
+
+
